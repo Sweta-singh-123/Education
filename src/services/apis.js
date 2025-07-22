7797be680f5fb0ee36636e42bd2b307755ae2623
Sweta <Sweta-singh-123@gmail.com>
2025-07-22 15:40:27 +0545

    improved

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 // const BASE_URL = process.env.REACT_APP_API_BASE_URL||"http://localhost:4000/api/v1"
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL||"http:localhost:4000/api/v1"
+const BASE_URL = process.env.REACT_APP_API_BASE_URL||"https://education-nx1i.onrender.com"
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
